a9f1d03220f1a0c2f9efb893be84f5f85f5ccc5e
matanc24 <matanc24@gmail.com>
2022-07-10 13:04:16 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum itaque
+        consequuntur cupiditate ducimus aut voluptas fugiat deserunt. Magni
+        omnis quia, velit distinctio voluptatibus fuga illo alias esse dolorem
+        harum exercitationem similique, voluptates, reiciendis aperiam! Fugit,
+        alias delectus! Sunt, itaque ipsum!
+      </p>
+    </section>
+  );
+};
+
+export default About;
